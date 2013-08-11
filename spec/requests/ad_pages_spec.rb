1db32fb5a0e2b88891399c1f7ecbaa841e392dd0
artem <artemts11@gmail.com>
2013-08-11 20:44:08 +0300

    Improve ad pages spec

--- spec/requests/ad_pages_spec.rb
+++ spec/requests/ad_pages_spec.rb
@@ -1,12 +1,6 @@
 require 'spec_helper'
 require "support/shared_examples_for_pages"
 
-shared_examples "a page bla" do
-  it { should have_link("Home", root_path) }
-  it { should have_link("Users", "#") }
-  it { should have_link("Sign in", "#") }
-end
-
 describe "Ad pages" do
   subject { page }
 
@@ -19,7 +13,7 @@ describe "Ad pages" do
                               :text => "Sign in") }
     it { should have_selector("a.btn.btn-large.btn-primary",
                               :text => "Sign up") }
-    it_should_behave_like "a page bla"
+    it_should_behave_like "a page header"
     it_should_behave_like "a page footer"
   end
 end
