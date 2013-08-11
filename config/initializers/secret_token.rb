ef38578dd011d5016283602f12ef1f63fb4b0c20
artem <artemts11@gmail.com>
2013-08-11 10:30:19 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JtTest::Application.config.secret_key_base = '796b21a3a06d562065fd3553b180ed9fbf6283620b2ef71e9050cd9dae8b714e628faed71219e58a8fa063ac17e81472d6efc9bac5a3d7c5fc3c9f6a19cbe7d8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JtTest::Application.config.session_store :cookie_store, key: '_jt_test_session'
